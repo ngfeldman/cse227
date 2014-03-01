22b24dfb5ce39f4e77c0426cd5f7070614714370
Nathan Feldman <nathangfeldman@gmail.com>
2014-02-28 22:09:22 -0800

    parallelization working... i think

--- src/crawler/netlog.js
+++ src/crawler/netlog.js
@@ -1,3 +1,5 @@
+var CONCURRENT_PAGES = 50;
+
 function dbLog(rt, msg, url, pt) {
   var xhr = new XMLHttpRequest();
   xhr.open("POST","http://localhost/Tracking/dbLog.php",true);
@@ -23,83 +25,106 @@ function getRandomInt (min, max) {
 }
 
 
-var page = require('webpage').create(),
-  system = require('system'),
-  address;
+var system = require('system');
 
 var addresses;
 
-page.open("top-1m.html", function(status) { 
-  addresses = page.plainText.split(' ');
-  page.release();
+var alexa_page = require('webpage').create();
+alexa_page.open("top-1m.html", function(status) { 
+  addresses = alexa_page.plainText.split(' ');
+  alexa_page.release();
+  iHateJavaScript();
 });
 
+function iHateJavaScript() {
+  var pagelist = new Array();
 
-/*  
-if (system.args.length === 1) {
-  console.log('Usage: netlog.js <some URL>');
-  phantom.exit();
-}  
-*/
 
-page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36';
-
-page.onResourceRequested = function(request) {
-  var msg = JSON.stringify(request, undefined, 4);
-  console.log('Request ' + msg);
-  dbLog(2, request, address, 'request');
-};
-page.onResourceReceived = function(response) {
-  var msg = JSON.stringify(response, undefined, 4);
-  console.log('Response ' + msg);
-  dbLog(2, response, address, 'response');
-};
-
-address = system.args[1];
-dbLog(1, address);
-
-page.open(address, function(status) {
-  if (status !== 'success') {
-    console.log('FAIL to load the address');
-    phantom.exit(1);
+  for (var i=0; i < CONCURRENT_PAGES && i < addresses.length; i++) {
+    processPage(addresses[i], pagelist, i)
   }
-  else {
-    console.log('Initial page load complete');
 
-    var coordsx = 0;
-    var coordsy = 0;
-    var count = 0;
+  var next_i = CONCURRENT_PAGES;
+  var active = CONCURRENT_PAGES;
+
+  function processPage(address, pagelist, i) {
+    var page = require('webpage').create();
+
+    pagelist[i] = page;
     
-    setTimeout(function() {
-      setInterval(function() {
-        for(i = 0; i<10; i++)
-        {
-          /*
-          coordsy += getRandomInt(1,10);
-          coordsx += getRandomInt(1,10);
-          if(coordsx > 1500) coordsx = 0;
-          if(coordsy > 1500) coordsy = 0;
-          */
-          if (count % 2 == 0) {
-            coordsx = 500;
-            coordsy = 125;
-          }
-          else {
-            coordsx = 5;
-            coordsy = 5;
-          }
-            
-          //console.log('['+count+'] moving mouse to ('+coordsx+','+coordsy+')');
-          page.sendEvent('mousemove', coordsx, coordsy);
+    page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36';
 
-          //if(coords % 30 == 0) {
-            //page.sendEvent('click', coords, coords, 'left');
-          //}
-          count++;
-        }
-      }, 10);
-    }, 250);
+    page.onResourceRequested = function(request) {
+      var msg = JSON.stringify(request, undefined, 4);
+      console.log('Request ' + msg);
+      dbLog(2, request, address, 'request');
+    };
+    page.onResourceReceived = function(response) {
+      var msg = JSON.stringify(response, undefined, 4);
+      console.log('Response ' + msg);
+      dbLog(2, response, address, 'response');
+    };
 
-    setTimeout(function(){ phantom.exit();}, 5250);
+    dbLog(1, address);
+
+    page.open('http://'+address, function(status) {
+      if (status !== 'success') {
+        console.log('FAIL to load the address ' + address);
+        phantom.exit(1);
+      }
+      else {
+        console.log('Initial page load of ' + address + ' complete');
+
+        var coordsx = 0;
+        var coordsy = 0;
+        var count = 0;
+        
+        var interval_id;
+        
+        setTimeout(function() {
+          interval_id = setInterval(function() {
+            for(i = 0; i<10; i++)
+            {
+              /*
+              coordsy += getRandomInt(1,10);
+              coordsx += getRandomInt(1,10);
+              if(coordsx > 1500) coordsx = 0;
+              if(coordsy > 1500) coordsy = 0;
+              */
+              if (count % 2 == 0) {
+                coordsx = 500;
+                coordsy = 125;
+              }
+              else {
+                coordsx = 5;
+                coordsy = 5;
+              }
+                
+              //console.log('['+count+'] moving mouse to ('+coordsx+','+coordsy+')');
+              page.sendEvent('mousemove', coordsx, coordsy);
+
+              //if(coords % 30 == 0) {
+                //page.sendEvent('click', coords, coords, 'left');
+              //}
+              count++;
+            }
+          }, 10);
+        }, 250);
+
+        setTimeout(function(){
+          var my_next_i = next_i++; //this all needs to happen at once for concurrency reasons. this was the shortest i could make it.
+          clearInterval(interval_id);
+          page.release();
+          if (my_next_i < addresses.length) {
+            proccessPage(addresses[i], pagelist, i);
+          } else {
+            --active;
+            if (active == 0) {
+              phantom.exit();
+            }
+          }
+        }, 5250);
+      }
+    });
   }
-});
+}
