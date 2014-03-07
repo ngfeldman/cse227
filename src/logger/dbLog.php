8df9afccad0f309cccc5d97ecc831de638a11c06
Scott Finney <scott.t.finney@gmail.com>
2014-03-07 07:00:30 -0800

    Updated for buffered logging
    Accepts requests containing multiple network records. Splits and logs
    them as appropriate in DB.

--- src/logger/dbLog.php
+++ src/logger/dbLog.php
@@ -1,12 +1,4 @@
 <?php
-function postVal($key) {
-	$val = isset($_POST[$key]) ? ($_POST[$key]) : "";
-  if (!empty($val) && is_string($val)) {
-    return str_replace(array('\\', "\0", "\n", "\r", "'", '"', "\x1a"), array('\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z'), $val);
-  } 
-	return($val);
-}
-
 function urlTrim($str) {
 	$url = trim($str);
 	$url = str_replace("http://", "", $url);
@@ -38,69 +30,67 @@ if (!function_exists('json_last_error_msg')) {
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	// connect
 	$m = new MongoClient();
-	$db = $m->Tracking;
+	$db = $m->Tracking3;
 	
-	$recordType = postVal("rt");
+	$stuff = explode(";;;;", $_POST['x']);
+	foreach ($stuff as $item) {
+		$record = json_decode(urldecode($item), true);
 		
-	if ($recordType == "1") {	// initial site connection
-		// add record
-		$url = urlTrim(postVal("u"));
-		$time = postVal("t");
-		
-		$collection = $db->sites;
-		if ($collection->find(array("url" => $url))->count() < 1) {
-			$doc = array('url' => $url, 'time' => $time);
+		$recordType = $record['rt'];
+			
+		if ($recordType == "1") {	// initial site connection
+			// add record
+			$url = urlTrim($record['u']);
+			$time = floatval($record['t']);
+			$sitenum = intval($record['d']);
+			
+			file_put_contents("logg.txt", "\nurl: " . $url . "\ttime: " . $time . "\n", FILE_APPEND | LOCK_EX);
+			
+			$collection = $db->sites;
+			if ($collection->find(array("url" => $url))->count() < 1) {
+				//file_put_contents("logg.txt", "adding $url to db\n", FILE_APPEND | LOCK_EX);
+				$doc = array('url' => $url, 'time' => $time, 'sitenum' => $sitenum);
+				$collection->insert($doc);
+			}
+			else {
+				file_put_contents("logg.txt", "found $url match in db already\n", FILE_APPEND | LOCK_EX);
+			}
+		}
+		else if ($recordType == "2") {	// network traffic record
+			$url = urlTrim($record['u']);
+			$time = floatval($record['t']);
+			$pt = $record['pt'];
+			$data = $record['d'];
+			//$err = json_last_error_msg();
+			
+			//get id
+			$collection = $db->sites;
+			$doc = $collection->findOne(array('url'=>$url), array('_id'));
+			$id = $doc['_id'];
+			//file_put_contents("logg.txt", "\nurl: " . $url . "\tsiteid: " . $id . "\ttime: " . $time . "\tdataDecoded: " . substr(urldecode($_POST['d']),0,50) . "\n", FILE_APPEND | LOCK_EX);
+			
+			//add record
+			$collection = $db->netlog;
+			$doc = array('siteId'=>$id, 'time'=> $time, 'packetType'=> $pt, 'data'=>$data);
+			$collection->insert($doc);
+		}
+		else if ($recordType == "3") {	// status message
+			$url = urlTrim($record['u']);
+			$time = floatval($record['t']);
+			$data = intval($record['d']);
+					
+			//get id
+			$collection = $db->sites;
+			$doc = $collection->findOne(array('url'=>$url), array('_id'));
+			$id = $doc['_id'];
+			
+			//file_put_contents("logg.txt", "\nurl: " . $url . "\tsiteid: " . $id . "\ttime: " . $time . "\tstatus: " . $data . "\n", FILE_APPEND | LOCK_EX);
+			
+			//add record
+			$collection = $db->netlog;
+			$doc = array('siteId'=>$id, 'time'=> $time, 'packetType'=> "", 'data'=>$data);
 			$collection->insert($doc);
 		}
 	}
-	else if ($recordType == "2") {	// network traffic log
-		$url = urlTrim(postVal("u"));
-		$time = postVal("t");
-		$pt = postVal("pt");
-		$data = json_decode(urldecode($_POST['d']), true);
-		$err = json_last_error_msg();
-		
-		//get id
-		$collection = $db->sites;
-		$doc = $collection->findOne(array('url'=>$url), array('_id'));
-		$id = $doc['_id'];
-		//file_put_contents("logg.txt", "\nurl: " . $url . "\njson err: " . $err . "\nsiteid: " . $id . ", time: " . $time . "\ndata: " . $_POST['d'] . "\ndataDecoded: " . urldecode($_POST['d']) . "\n", FILE_APPEND | LOCK_EX);
-		
-		//add record
-		$collection = $db->netlog;
-		$doc = array('siteId'=>$id, 'time'=> $time, 'packetType'=> $pt, 'data'=>$data);
-		$collection->insert($doc);
-		
-		/*
-		$data = array(
-			"siteId" => postVal("site"),		// which site this record is for
-			"type" => postVal("type"),			// type of network traffic (request or response)
-			"requestId" => postVal("id"),		// sequential id associated with requests
-			"method" => postVal("method"),	// POST or GET (for requests)
-			"time" => postVal("time"),			// time of packet
-			"url" => postVal("url"), 				// url of packet
-			"size" => postVal("size"), 			// size of packet (for responses)
-			"contentType" => postVal("contentType"),
-			"status" => postVal("status"),
-			"stage" => postVal("stage")
-		);
-		*/
-	}
-	else if ($recordType == "3") {	// status message
-		$url = urlTrim(postVal("u"));
-		$time = postVal("t");
-		$data = postVal("d");
-				
-		//get id
-		$collection = $db->sites;
-		$doc = $collection->findOne(array('url'=>$url), array('_id'));
-		$id = $doc['_id'];
-		
-		//add record
-		$collection = $db->netlog;
-		$doc = array('siteId'=>$id, 'time'=> $time, 'packetType'=> "", 'data'=>$data);
-		$collection->insert($doc);
-	}
 }
 ?>
-ok
