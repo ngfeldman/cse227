6cdaa0ddc5d4ce0bc21bacc015da97322c3aabf7
Scott Finney <scott.t.finney@gmail.com>
2014-03-09 13:54:44 -0700

    Added logging type and keypress arguments

--- src/crawler/netlog_win.ps1
+++ src/crawler/netlog_win.ps1
@@ -1,6 +1,6 @@
 $batch=50
 $parallel=10
-$(foreach ($i in 0..4) { 
+$(foreach ($i in 0..0) {
    $j= $i * $batch
-   phantomjs netlog.js $parallel $j $batch
+   phantomjs netlog.js $parallel $j $batch keypress file
 })
